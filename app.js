f84447dfb18d3e8313916b1cee479fb0508b64ba
Peter Eck <jpeckiii@gmail.com>
2018-06-22 20:40:42 -0500

    More style and generate clienToken successful

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var express         = require("express"),
     session         = require("express-session"),
     braintree       = require("braintree"),
-    client          = require("braintree-web/client"),
-    hostedFields    = require("braintree-web/hosted-fields"),
     flash           = require("connect-flash"),
     bodyParser      = require("body-parser");
     path            = require("path");
@@ -10,15 +8,15 @@ var express         = require("express"),
 
 
 // Setting up the app
+app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json())
 app.use(flash());
-app.set("views", path.join(__dirname, "/views"));
 app.use(express.static(__dirname + "/public"));
 
 app.use(session({
-    secret: process.env.clientSecret,
+    secret: "my_secret",
     saveUninitialized: true,
     resave: true
 }));
@@ -32,72 +30,54 @@ app.use(function(req, res, next) {
 // Creating gateway with Sandbox credentials
 var gateway = braintree.connect({
     environment: braintree.Environment.Sandbox,
-    merchantId: process.env.merchantID,
-    publicKey: process.env.publicKey,
-    privateKey: process.env.privateKey
+    merchantId: "7nzxrq4pc2ttg38j",
+    publicKey: "9p82fdb76b85n64w",
+    privateKey: "a6fc1c85412a8ee854bcc098e60a31ad"
 });
 
 app.get("/", function(req, res) {
-    res.redirect("/checkout");
+    res.render("home");
 });
 
 // Send client token to the client
 app.get("/checkout", function(req, res) {
-    gateway.clientToken.generate({}, function(generateError, response) {
-        if(generateError) {
+    gateway.clientToken.generate({}, function(error, response) {
+        var clientToken = response.clientToken;
+        if(error) {
             // Handle error
-            console.log(generateError);
+            console.log(error);
         }
-        // res.send(response.clientToken)
-        res.render("checkout", {clientToken: response.clientToken});
-        console.log(response.clientToken);
+        res.render("checkout", { clientToken: clientToken });
     });
 });
 
 app.post("/checkout", function(req, res) {
-    var transactionError;
-    var nonce = req.body.payment_method_nonce;
-    var amount = req.body.amount;
-    var firstName = req.body.firstName;
-    var lastName = req.body.lastName;
-
+    var nonceFromTheClient = req.body.payment_method_nonce;
+    console.log("The nonce is: " + nonceFromTheClient);
     gateway.customer.create({
-        firstName: firstName,
-        lastName: lastName,
-        paymentMethodNonce: nonce,
-        creditCard: {
-            options: {
-                verifyCard: true
-            }
+        firstName: "New",
+        lastName: "Customer",
+        paymentMethodNonce: nonceFromTheClient,
+        options: {
+            verifyCard: true,
         }
-    }, function(createCustomerError, createCustomerResult) {
-        if(createCustomerError) {
-            console.log(createCustomerError);
-            // Handle error
+    }, function(error, result) {
+        if(error) {
+            console.log("Customer create error");
             return;
         }
-        else if (!createCustomerResult.success) {
-            console.log("There was an error creating the customer");
-        }
-        else {
+        console.log("The customer ID is: " + customerId);
 
-            gateway.transaction.sale({
-                amount: amount,
-                paymentMethodNonce: nonce,
-                options: {
-                    verifyCard: true,
-                    amount: amount
-                }
-            }, function (saleError, result) {
-                if (result.success) {
-                    // Result.transaction for details
-                } else {
-                    console.log(saleError);
-                    // Handle errors
-                }
-            });
-        } 
+        gateway.transaction.sale({
+            amount: "$10",
+            paymentMethodNonce: result.customer.paymentMethods[0].token,
+            options: {
+                submitForSettlement: true,
+                storeInVaultOnSuccess: true
+            }
+        });
     });
+    res.render("confirmation");
 });
 
 app.listen(3000, function(req, res) {
--- controllers/main.js
+++ controllers/main.js
@@ -1,71 +1,56 @@
-var braintree = require("braintree");
+var form = document.querySelector("#card-form");
+var submit = document.querySelector('input[type="submit"]');
+var nonce = document.querySelector("#nonce");
 
-
-initializeBraintree = function() {
-    var form = document.querySelector("#card-form");
-    var token = "";
-
+function initializeBraintree() {
     braintree.client.create({
-        authorization: "CLIENT_AUTHORIZATION"
+        authorization: "sandbox_t4qgfw7s_7nzxrq4pc2ttg38j",
     }, function (clientError, clientInstance) {
         if (clientError) {
-            console.log(clientError);
             // Handle error
+            console.log("Client error!");
             return;
         }
         braintree.hostedFields.create({
             client: clientInstance,
-            styles: {},
             fields: {
                 number: {
                     selector: "#card-number",
-                    palceholder: "5430 6950 0294 9750"
-                },
-                expirationMonth: {
-                    selector: "#exp-month",
-                    placeholder: "MM"
-                },
-                expirationYear: {
-                    selector: "#exp-year",
-                    placeholder: "YYYY"
+                    placeholder: "4111 1111 1111 1111"
                 },
                 cvv: {
                     selector: "#cvv",
-                    placeholder: "456"
+                    placeholder: "750"
+                },
+                expirationDate: {
+                    selector: "#expiration-date",
+                    placeholder: "12/2021"
                 },
                 postalCode: {
                     selector: "#postal-code",
-                    placeholder: "12345"
-                },
-                amount: {
-                    selector: "#amount",
-                    placeholder: "0.0"
+                    placeholder: "60606"
                 }
             }
         }, function (hostedFieldsError, hostedFieldsInstance) {
-            if (hostedFielsdError) {
-                console.log(hostedFieldsError)
+            if (hostedFieldsError) {
                 // Handle error
+                console.log("Hosted Fields Error!");
                 return;
             }
-            submit.removeAttribute("disable");
-
+            submit.removeAttribute("disabled");
             form.addEventListener("submit", function (event) {
                 event.preventDefault();
 
                 hostedFieldsInstance.tokenize(function (tokenizeError, payload) {
                     if (tokenizeError) {
-                        console.log(tokenizeError);
                         // Handle error
+                        console.log("Tokenize Error!");
                         return;
                     }
                     console.log("The nonce is: " + payload.nonce);
-                    document.querySelector("#nonce").value = payload.nonce;
-                    form.submit();
+                    nonce.setAttribute("value", payload.nonce);
                 });
             }, false);
         });
     });
 }
-
-
