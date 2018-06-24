da509df7f8d6949cdc8fcd171af1648f97cbd7e9
Peter Eck <jpeckiii@gmail.com>
2018-06-24 16:28:19 -0500

    Flash messages working

--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,7 @@ router.get("/checkout", function (req, res) {
         if (error) {
             console.log(error);
         }
-        res.render("checkout", { clientToken: clientToken });
+        res.render("checkout", { clientToken: clientToken, message: req.flash("error") });
     });
 });
 
